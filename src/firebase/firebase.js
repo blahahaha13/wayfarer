30d3df6a8d774884abe8ec884555673a0524ea71
praveenkhanal <praveen_fren@hotmail.com>
2018-11-14 12:36:34 -0800

    started authentication

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyCsE02H1lFtW2RaRygITvr-uPUySj6aNco",
+  authDomain: "wayfarer-38067.firebaseapp.com",
+  databaseURL: "https://wayfarer-38067.firebaseio.com",
+  projectId: "wayfarer-38067",
+  storageBucket: "wayfarer-38067.appspot.com",
+  messagingSenderId: "468268311649"
+  };
+if (!firebase.apps.length){
+  firebase.initializeApp(config);
+}
+const auth = firebase.auth();
+export {
+  auth,
+};
