fc426b3b116d977bec99037bf30f145008b6bb76
blahahaha13 <gmony1558@gmail.com>
2018-11-15 10:29:48 -0800

    fixed tabbing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 import withAuthorization from './withAuthorization';
 const HomePage = () =>
 <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+  <h1>Home Page</h1>
+  <p>The Home Page is accessible by every signed in user.</p>
 </div>
 
 const authCondition = (authUser) => !!authUser;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const LandingPage = () =>
 <div>
-    <h1>Landing Page</h1>
+  <h1>Landing Page</h1>
 </div>
 
 export default LandingPage;
